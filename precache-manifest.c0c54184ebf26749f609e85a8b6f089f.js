c614dc0244862a4a70b3e100c8cbac8a9f213aea
Tze Yang Chew <tzeyangchew@MacBook-Pro.local>
2019-03-05 23:02:49 +0800

    Updates

---
+++ precache-manifest.c0c54184ebf26749f609e85a8b6f089f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "900838cca74a536f1fda",
+    "url": "/React-movies-list/static/js/runtime~main.900838cc.js"
+  },
+  {
+    "revision": "777ed5855dfbe6478c7e",
+    "url": "/React-movies-list/static/js/main.777ed585.chunk.js"
+  },
+  {
+    "revision": "d202bd078bbcac00b653",
+    "url": "/React-movies-list/static/js/2.d202bd07.chunk.js"
+  },
+  {
+    "revision": "777ed5855dfbe6478c7e",
+    "url": "/React-movies-list/static/css/main.f2828e64.chunk.css"
+  },
+  {
+    "revision": "99dcc86037bf80e3c62f2074e6be0339",
+    "url": "/React-movies-list/index.html"
+  }
+];
